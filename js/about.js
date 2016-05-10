dcf9fd23cc59ffe3a25b0d249bab471543126c9a
Bharath ツ <bharathraja@live.com>
2016-05-11 02:38:37 +0530

    about - landing screen responsive finish

--- js/about.js
+++ js/about.js
@@ -1,4 +1,67 @@
+$('.first-section, .me-love').css({ height: $(window).height() })
+
+// Initiate ILT animation
+setTimeout(function() {
+  $('.ilt').addClass('animate')
+}, 4000)
+
+// Randomize description
+;(function() {
+  var desc = [
+    'I\’m a designer who can code.'
+    , 'Designer by skill, Hacker by nature and Artist by heart.'
+    , 'Geek. Gamer. Rookie artist. Weird thinker and a bathroom singer.'
+  ]
+  $('#desc').html(desc[Math.floor(Math.random() * desc.length)])
+})()
+
 $(function() {
-  // $('.header').addClass('minimized')
-  // $('.icon-container').addClass('placed-top')
+
+  $(window).on('resize', function(event) {
+    $('.first-section, .me-love').css({ height: $(window).height() })
+  })
+
+  var top = true
+  $(window).on('scroll resize', function(event) {
+
+    if ($(window).scrollTop() < 30) {
+      !top && $('.scroll-down-arrows').removeClass('hide');
+      !top && $('.header').removeClass('minimized')
+      !top && $('.icon-container').removeClass('placed-top')
+      top = true
+    } else {
+      top && $('.scroll-down-arrows').addClass('hide');
+      top && $('.header').addClass('minimized')
+      top && $('.icon-container').addClass('placed-top')
+      top = false
+    }
+  })
+
+  $('.scroll-down-arrows').click(function() {
+    $('body').animate({
+      scrollTop: $('.first-section').height(),
+    }, 1000)
+  })
+
+
+  $('.ilt-holder')
+    .mouseover(function() {
+      $('.ilt').removeClass('animate')
+    })
+    .mouseout(function() {
+      $('.ilt').addClass('animate')
+    })
+
+  $('.ilt-option')
+    .mouseover(function() {
+      $('.ilt').css({ top: $(this).index() * 20 + '%' })
+    })
+    .click(function() {
+      var submenu = $(this).attr('data-id')
+      window.location.hash = submenu
+      $('body').scrollTop(0)
+      $('body').animate({
+        scrollTop: $('#' + submenu).offset().top,
+      }, 1000)
+    })
 })
